537dbce957c8742d25b5893c188d16f39b547aa2
Juen Khaw <khawtattjuen1997@gmail.com>
2019-03-24 15:02:32 +0800

    Added displaying graph for loss and accuracy

--- demo_misc.py
+++ demo_misc.py
@@ -5,6 +5,7 @@ Created on Sat Mar 16 17:11:52 2019
 @author: Juen
 """
 import numpy as np
+import matplotlib.pyplot as plt
 
 def get_class_label(mapfile_path):
     # read in the UCF class labels for visualization purpose
@@ -19,3 +20,41 @@ def get_prediction(scores, top_k, class_label):
     scores = scores.cpu().detach().numpy()
     indices = np.argsort(scores, axis = 1)[0, (101 - top_k) :][::-1]
     return {class_label[index] : scores[0, index] for index in indices}
+
+def plotlossgraph(data, x_label, y_label, legends, cap = 45):
+    x = list(range(cap))
+    plotline = ['r--', 'b--', 'g--', 'k--']
+    handles = []
+    
+    assert(len(data) < 5)
+    
+    for i in range(len(data)):
+        y = data[i][:cap]
+        h, = plt.plot(x, y, plotline[i], label = legends[i])
+        handles.append(h)
+    
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend(handles = handles)
+    plt.show()
+    
+def plotaccgraph(data, x_label, y_label, legends, cap = 45):
+    x = list(range(cap))
+    trainline = ['r--', 'b--', 'g--']
+    testline = ['r', 'b', 'g']
+    handles = []
+    
+    assert(len(data) < 6)
+    
+    for i in range(len(data) // 2):
+        train_acc = data[i * 2][:cap]
+        test_acc = [data[i * 2 + 1] for j in range(cap)]
+        h, = plt.plot(x, train_acc, trainline[i], label = legends[i * 2])
+        handles.append(h)
+        h, = plt.plot(x, test_acc, testline[i], label = legends[i * 2 + 1])
+        handles.append(h)
+    
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.legend(handles = handles)
+    plt.show()
